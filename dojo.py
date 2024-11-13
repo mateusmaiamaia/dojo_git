69b7822535eb8f5a77efe3c18ce42de96f6d8cd2
mateusmaiamaia <mateusmaiamaia@hotmail.com>
2024-11-12 21:18:14 -0300

    refactor

--- dojo.py
+++ dojo.py
@@ -2,7 +2,7 @@ print("Oi, esse é o dojo de git da equipe 1 de EPS/MDS!")
 
 print("Por favor, mude a variável 'nome' para o seu nome")
 
-nome = "Paulin"
+nome = "Mateus Maia"
 
 print(f"Tudo certo {nome}! Pode seguir para o próximo passo do dojo!")
 
